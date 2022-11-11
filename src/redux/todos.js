f792b3d404fd232f88f89ef6296f31438698db75
aleksei-v <aleksey.vasilenko7@gmail.com>
2022-11-11 11:36:13 +0200

    Update todos.js

--- src/redux/todos.js
+++ src/redux/todos.js
@@ -1,4 +1,4 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
 export const todosApi = createApi({
   reducerPath: 'todosApi',
@@ -12,7 +12,7 @@ export const todosApi = createApi({
     deleteTodo: builder.mutation({
       query: todoId => ({
         url: `/todos/${todoId}`,
-        method: 'DELETE',
+        method: 'DELETE',  
       }),
       invalidatesTags: ['Todo']
     }),
