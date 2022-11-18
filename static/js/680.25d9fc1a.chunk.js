d7da6ac4ba706a711657acd62e6b7d4dc984e2ce
aleksei-v <95973001+aleksei-v@users.noreply.github.com>
2022-11-18 12:17:43 +0000

    Deploying to gh-pages from @ aleksei-v/todo-application@9ee552beb2848f3eaf65e3403c4d2a1728f096c8 🚀

---
+++ static/js/680.25d9fc1a.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunktodo_application=self.webpackChunktodo_application||[]).push([[680],{680:function(n,e,l){l.r(e);var o=l(7683),t=l(184);e.default=function(){return(0,t.jsx)(o.x,{p:6,children:(0,t.jsx)("h1",{children:"Hello World"})})}}}]);
+//# sourceMappingURL=680.25d9fc1a.chunk.js.map
